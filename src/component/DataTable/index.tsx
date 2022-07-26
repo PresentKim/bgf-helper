5d0249e0399b2cb3debb8453cb2f83c5729bb741
PresentKim <debe3721@gmail.com>
2022-07-26 09:14:27 +0900

    :sparkles: Remove when long click row

--- src/component/DataTable/index.tsx
+++ src/component/DataTable/index.tsx
@@ -3,6 +3,7 @@ import SupportData from "../../data/SupportData";
 import styles from './index.module.css';
 import DataTableHead from "../DataTableHead";
 import Store from "../../data/Store";
+import {produce} from "solid-js/store";
 
 export default (props: { rows: SupportData[] }) => {
     const {rows} = props;
@@ -12,8 +13,16 @@ export default (props: { rows: SupportData[] }) => {
         <table class={styles.DataTable}>
             <DataTableHead/>
             <tbody>
-            <For each={rows}>{(data: SupportData) =>
-                <tr>
+            <For each={rows}>{(data: SupportData, i) =>
+                <tr oncontextmenu={(e) => {
+                    e.preventDefault();
+                    setStore(
+                        produce((params) => {
+                            const index = params.dataList.findIndex(data.equals.bind(data));
+                            params.dataList.splice(index, 1);
+                        })
+                    );
+                }}>
                     <td class={styles.dock}>{data.dock}</td>
                     <td class={styles.store}>{data.store}</td>
                     <td class={styles.name}>{data.name}</td>
